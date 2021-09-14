18cc782c2b46b336532ac5ce4030b0857bdb453d
Soumen <soumenkp2@gmail.com>
2021-09-14 21:18:11 +0530

    ADDED int card

--- app/src/main/java/com/example/birthday_card/card.java
+++ app/src/main/java/com/example/birthday_card/card.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class card extends AppCompatActivity {
 
+
+    int card;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
